c11b39035e948bd8542b83f68c1de5f79e9a7833
GitHub Action <action@github.com>
2021-07-12 19:05:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/nehapednekar/customplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nehapednekar/customplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nehapednekar.customplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nehapednekar.customplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nehapednekar/customplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nehapednekar/customplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nehapednekar.customplugin.services
 
+import com.github.nehapednekar.customplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
